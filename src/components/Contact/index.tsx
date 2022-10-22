b6b8e3d3be89273b112b8bab3be27e28e18b06aa
Evander Inacio <evander.20112@hotmail.com>
2022-10-21 22:23:42 -0300

    Contact :speech_balloon:

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,8 +1,8 @@
 export function Contact() {
   return (
     <form action="https://formsubmit.co/evander.20112@hotmail.com" method="POST">
-      <input placeholder="Nome" type="text" />
-      <input placeholder="email" type="email" />
+      <input placeholder="Nome" type="text" required/>
+      <input placeholder="email" type="email" required/>
       <textarea name="mensagem" id="mensagem" required></textarea>
 
       <button type="submit">enviar</button>
