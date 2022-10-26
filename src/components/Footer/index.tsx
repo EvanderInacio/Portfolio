178cf5056939dc9da69a16dad3f0b8b3dc53c174
Evander Inacio <evander.20112@hotmail.com>
2022-10-25 23:13:53 -0300

    Links :bookmark:

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,11 +6,23 @@ import { FooterContainer } from './styles'
 export function Footer() {
   return (
     <FooterContainer>
-      <h4> &copy; 2022 {' '} Evander Inácio</h4>
-      <div className='links'>
-        <Link href={""}><a><FiGithub /></a></Link>
-        <Link href={""}><a><FiLinkedin /></a></Link>
-        <Link href={""}><a><FaWhatsapp /></a></Link>
+      <h4> &copy; 2022 Evander Inácio</h4>
+      <div className="links">
+        <Link href={'https://github.com/EvanderInacio'}>
+          <a target="_blank">
+            <FiGithub />
+          </a>
+        </Link>
+        <Link href={'https://www.linkedin.com/in/evander-inacio/'}>
+          <a target="_blank">
+            <FiLinkedin />
+          </a>
+        </Link>
+        <Link href={'https://api.whatsapp.com/send?phone=5511995085916'}>
+          <a target="_blank">
+            <FaWhatsapp />
+          </a>
+        </Link>
       </div>
     </FooterContainer>
   )
--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -1,20 +1,39 @@
-import Link from "next/link"
-import { Icons } from "./styles"
+import Link from 'next/link'
+import { Icons } from './styles'
 import { FiGithub, FiLinkedin, FiFacebook, FiTwitter } from 'react-icons/fi'
 import { FaWhatsapp } from 'react-icons/fa'
 
-
 export function Links() {
   return (
-   <>
-     <Icons>
-        <Link href={""}><a><FiGithub /></a></Link>
-        <Link href={""}><a><FiLinkedin /></a></Link>
-        <Link href={""}><a><FiFacebook /></a></Link>
-        <Link href={""}><a><FiTwitter /></a></Link>
-        <Link href={""}><a><FaWhatsapp /></a></Link>
+    <>
+      <Icons>
+        <Link href={'https://github.com/EvanderInacio'}>
+          <a target="_blank">
+            <FiGithub />
+          </a>
+        </Link>
+        <Link href={'https://www.linkedin.com/in/evander-inacio/'}>
+          <a target="_blank">
+            <FiLinkedin />
+          </a>
+        </Link>
+        <Link href={'https://www.facebook.com/evandder.lopes/'}>
+          <a target="_blank">
+            <FiFacebook />
+          </a>
+        </Link>
+        <Link href={'https://twitter.com/Evander_Inacio'}>
+          <a target="_blank">
+            <FiTwitter />
+          </a>
+        </Link>
+        <Link href={'https://api.whatsapp.com/send?phone=5511995085916'}>
+          <a target="_blank">
+            <FaWhatsapp />
+          </a>
+        </Link>
         <div className="barra"></div>
       </Icons>
-   </>
+    </>
   )
 }
