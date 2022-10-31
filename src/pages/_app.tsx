e67a351ee33ecbe6ad43c73e0316210d84f04f40
Evander Inacio <evander.20112@hotmail.com>
2022-10-31 20:06:21 -0300

    App :test_tube:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,32 @@
-import type { AppProps } from 'next/app'
-// import { useRouter } from 'next/router';
-// import { useEffect } from 'react'
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/global';
 
-// function useNormalScrollRoutes() {
-//   const router = useRouter();
+function useNormalScrollRoutes() {
+  const router = useRouter();
 
-//   useEffect(() => {
-//     router.events.on('routeChangeStart', () => {
-//       document.documentElement.classList.add('normal-scroll');
-//     });
-//     router.events.on('routeChangeComplete', () => {
-//       document.documentElement.classList.remove('normal-scroll');
-//     });
-//   // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-// }
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      document.documentElement.classList.add('normal-scroll');
+    });
+    router.events.on('routeChangeComplete', () => {
+      document.documentElement.classList.remove('normal-scroll');
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // useNormalScrollRoutes();
+  useNormalScrollRoutes();
+
   return (
     <ThemeProvider theme={GlobalStyles}>
         <Component {...pageProps} />
         <GlobalStyles />
     </ThemeProvider>
-  )
-}
+  );
+};
 
 export default MyApp
